98bc24a825b8da96b9caf5c394dc293e772f9dd6
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2023-02-16 12:24:47 +0100

    Run checks, fix bugs and add eigen-value decomposition to simulate function.

--- R/simulate_cMTS.R
+++ R/simulate_cMTS.R
@@ -2,14 +2,14 @@
 #' @import stats
 #' @import longmemo
 #' @param N Length of Times Series
-#' @param H Hurst Exponents for `d` time series. These are then mixed using the Cholesky decomposition of the given covariance matrix `Sigma`.
-#' @param Sigma Positive semi definite covariance matrix of desired multi dimensional time series.
+#' @param H Hurst Exponents for \code{d} time series. These are then mixed using one of two different decompositions of the given covariance matrix \code{Sigma}.
+#' @param Sigma Positive semi definite covariance matrix of desired multi-dimensional time series.
 #' @param simulation_process The simulation process passed to the \code{longmemo::sim...} function. Can either be \code{longmemo::simFGN.fft} (using FFT) or \code{longmemo::simFGN0} (using fractional gaussian proccesses). FGN0 looks more like \code{rnorm}, when \code{H=0.5}. \code{DEFAULT} to \code{"FGN0"}. Use \code{simulation_process="FGN.fft"} to use the FFT based version.
-#' @param chol Character whether the Cholesky decomposition \code{"chol"} (or \code{"cholesky"}) should be used or whether the eigen decomposition should be used (\code{chol = "eigen"}). \code{DEFAULT} to \code{"chol"}.
-#' @return Returns a multivariate correlated time series with covariance matrix `Sigma`. The Hurst exponents are only approximate univariatly, since they result from mixed time series. Uncorrelated time series keep their univariate Hurst exponent `H`.
+#' @param decomposition Character whether the Cholesky decomposition \code{"chol"} (or \code{"cholesky"}) should be used or whether the eigen decomposition should be used (\code{chol = "eigen"}). \code{DEFAULT} to \code{"chol"}.
+#' @return Returns a multivariate correlated time series with covariance matrix \code{Sigma}. The Hurst exponents are only approximate univariatly, since they result from mixed time series. Uncorrelated time series keep their univariate Hurst exponents \code{H}.
 #' @export
 
-simulate_cMTS <- function(N, H, Sigma, simulation_process = "FGN0", chol = "chol")
+simulate_cMTS <- function(N, H, Sigma, simulation_process = "FGN0", decomposition = "chol")
 {
      ### check input ---
      if(!simulation_process %in% c("FGN0", "FGN.fft")) stop("simulation_process can either be FGN0 or FGN.fft")
@@ -33,7 +33,7 @@ simulate_cMTS <- function(N, H, Sigma, simulation_process = "FGN0", chol = "chol
 
 
      ### generate intercorrelated variables from independent ones
-     if(tolower(chol) %in% c("chol", "cholesky")){
+     if(tolower(decomposition) %in% c("chol", "cholesky")){
           X <- data.frame(as.matrix(Xi) %*% chol(Sigma))
      }else{
           ew <- eigen(Sigma)
