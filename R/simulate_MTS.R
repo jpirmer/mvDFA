ee6e2d53f546af5abb6726cccf2d92a7ebb53719
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2023-06-02 11:58:04 +0200

    Add option to correlate increments and give disclaimers.

--- R/simulate_MTS.R
+++ R/simulate_MTS.R
@@ -1,59 +1,88 @@
-#' Approximate correlated time series from white, pink and brown noise
+#' Approximate correlated time series from white, pink and brown noise from independent realization of normal variates
 #' @import stats
 #' @import RobPer
 #' @import mvtnorm
 #' @param N Length of multivariate Times Series
 #' @param Sigma Positive semi definite covariance matrix the increments of desired multi dimensional time series. The dimensionality of Sigma sets the dimension of the time series. The variance scale the time. If the variances are all 1, then each data point represents one unit of time.
 #' @param process Type of process. Can either be "white", "brown" or "pink". Default to "white". If process is a vector, a mixture of the three process is generated, correlated by Sigma.
+#' @param decomposition Character whether the Cholesky decomposition \code{"chol"} (or \code{"cholesky"}) should be used or whether the eigen decomposition should be used (\code{decomposition = "eigen"}). \code{DEFAULT} to \code{"chol"}.
+#' @param cor_increments Logical, whether to correlate the increments or the time series themselves. Default to \code{TRUE}.
+#' @param X0 Starting values for the time series if increments are correlated. Default to \code{rep(0, ncol(Sigma))}, i.e., the zero vector of required length.
+#' @description
+#' Approximation of correlated time series representing "white", "pink" or "brown" noise from independent realization of normal variates. Internally normal variables are simulated using \code{rnorm} and then are cumualted for white or brown noise and we use \code{RobPer::TK95} for the generation of pink noise. We cautiously note that we use empirical scaling (i.e., the variances are scaled to be 1 in the sample not the population), hence the between sample variance may be underrepresented. We further note that the covariance estimates for correlated time series (not using increments) is unstable.
+#'
+#'@examples
+#' Sigma <- matrix(.5, 3, 3); diag(Sigma) <- c(1,2,3)
+#' data <- simulate_MTS_mixed_white_pink_brown(N = 10^5, Sigma = Sigma,
+#'                                             process = c("white", "pink", "brown"),
+#'                                             cor_increments = FALSE)
+#' cov(data) # unstable covariances
+#' cov(apply(data,2,diff))
 #' @return Returns a multivariate correlated time series with covariance matrix `Sigma`. The Hurst exponents are only approximate univariatly, since they result from mixed time series. Here, a mixture of "white", "pink" and "brown" noise can be chosen from. Uncorrelated time series keep their univariate Hurst exponent `H`.
 #' @export
 
-simulate_MTS_mixed_white_pink_brown <- function(N, Sigma, process = "white")
+simulate_MTS_mixed_white_pink_brown <- function(N, Sigma, process = "white",decomposition = "chol", cor_increments = TRUE, X0 = rep(0, ncol(Sigma)))
 {
      ### check input ---
      if(!is.matrix(Sigma)) stop("Wrong format for Sigma. Needs to be a matrix.")
      if(!isSymmetric(Sigma) | any(eigen(Sigma)$value < 0)) stop("Sigma needs to be symmetric and positive semi definite.")
 
-     ### generate multivariate variables
-     if(length(process) > 1)
+     ### compute sqrt of Sigma:
+     if(tolower(decomposition) %in% c("chol", "cholesky")){
+         Sigma_sq <-  chol(Sigma)
+     }else if(tolower(decomposition) %in% c("eigen", "eigenvalue", "svd"))
      {
-             X <- matrix(NA, nrow = N, ncol = ncol(Sigma))
-             if(sum(process == "pink") > 0)
-             {
-                     P <- replicate(n = sum(process == "pink"), RobPer::TK95(N = (N+1))) # pink noise
-                     P_incr <- scale(apply(P, 2, diff), center = F); rm(P)       # increments
-                     X[, process == "pink"] <- P_incr; rm(P_incr)
-             }
-             if(any(c("white", "brown") %in% process))
-             {
-                     X[, process %in% c("white", "brown")] <- replicate(n = sum(process %in% c("white", "brown")),
-                                                                        expr = rnorm(n = N))
-             }
-             X <- as.matrix(X) %*% chol(Sigma)
-             if(any(c("pink", "brown") %in% process))
-             {
-                     X[, process %in% c("pink", "brown")] <- apply(as.matrix(X[, process %in% c("pink", "brown")]), 2, cumsum)
-             }
-     }else{
-             if(process == "white")
-             {
-                     X <- mvtnorm::rmvnorm(n = N, sigma = Sigma)
-             }else if(process == "brown"){
-                     Z <- mvtnorm::rmvnorm(n = N, sigma = Sigma)
-                     X <- apply(Z, 2, cumsum)
-             }else{
-                     P <- replicate(n = ncol(Sigma), RobPer::TK95(N = (N+1)))         # pink noise
-                     P_incr <- scale(apply(P, 2, diff), center = F); rm(P)    # increments
-                     X_incr <- P_incr %*% chol(Sigma); rm(P_incr)             # correlate increments
-
-                     # process can be rescaled via the increments
-                     X <- apply(X_incr, 2, cumsum); rm(X_incr)
-
-             }
+          ew <- eigen(Sigma)
+          Sigma_sq <- ew$vectors %*% diag(sqrt(ew$values)) %*% solve(ew$vectors)
      }
 
-     colnames(X) <- paste0("X", 1:ncol(Sigma))
+     ### generate multivariate variables
+     Xi <- matrix(NA, nrow = N, ncol = ncol(Sigma))
+     if(sum(process == "pink") > 0)
+     {
+          P <- replicate(n = sum(process == "pink"), RobPer::TK95(N = (N+1))) # pink noise
+          if(cor_increments)
+          {
+               P_incr <- scale(apply(P, 2, diff)); rm(P)       # increments
+               Xi[, process == "pink"] <- P_incr; rm(P_incr)
+          }else{
+               Xi[, process == "pink"] <- scale(P[-nrow(P),,drop = F])
+          }
 
-     class(X) <- c("MultiTimeseriesLongMemory", "matrix")
+     }
+     if(sum(process == "white") > 0)
+     {
+          W <- replicate(n = sum(process == "white"), rnorm(N+1)) # pink noise
+          if(cor_increments)
+          {
+               W_incr <- scale(apply(W, 2, diff)); rm(W)       # increments
+               Xi[, process == "white"] <- W_incr; rm(W_incr)
+          }else{
+               Xi[, process == "white"] <- scale(W[-nrow(W),,drop = F])
+          }
+
+     }
+     if(sum(process == "brown") > 0)
+     {
+          B <- apply(replicate(n = sum(process == "brown"), rnorm(N+1)), 2, cumsum) # pink noise
+          if(cor_increments)
+          {
+               B_incr <- scale(apply(B, 2, diff)); rm(B)       # increments
+               Xi[, process == "brown"] <- B_incr; rm(B_incr)
+          }else{
+               Xi[, process == "brown"] <- scale(B[-nrow(B),,drop = F])
+          }
+
+     }
+     X_temp <- Xi %*% Sigma_sq
+
+     if(cor_increments){
+          X <- apply(rbind(X0, X_temp), 2, cumsum)
+     }else{
+          X <- X_temp
+     }
+
+     X <- data.frame(X)
+     names(X) <- paste0("X", 1:ncol(X))
      return(X)
 }
--- R/simulate_cMTS.R
+++ R/simulate_cMTS.R
@@ -5,13 +5,22 @@
 #' @param H Hurst Exponents for \code{d} time series. These are then mixed using one of two different decompositions of the given covariance matrix \code{Sigma}.
 #' @param Sigma Positive semi definite covariance matrix of desired multi-dimensional time series.
 #' @param simulation_process The simulation process passed to the \code{longmemo::sim...} function. Can either be \code{longmemo::simFGN.fft} (using FFT) or \code{longmemo::simFGN0} (using fractional gaussian proccesses). FGN0 looks more like \code{rnorm}, when \code{H=0.5}. \code{DEFAULT} to \code{"FGN0"}. Use \code{simulation_process="FGN.fft"} to use the FFT based version.
-#' @param decomposition Character whether the Cholesky decomposition \code{"chol"} (or \code{"cholesky"}) should be used or whether the eigen decomposition should be used (\code{chol = "eigen"}). \code{DEFAULT} to \code{"chol"}.
+#' @param decomposition Character whether the Cholesky decomposition \code{"chol"} (or \code{"cholesky"}) should be used or whether the eigen decomposition should be used (\code{decomposition = "eigen"}). \code{DEFAULT} to \code{"chol"}.
+#' @param cor_increments Logical, whether to correlate the increments or the time series themselves. Default to \code{TRUE}.
+#' @param X0 Starting values for the time series if increments are correlated. Default to \code{rep(0, ncol(Sigma))}, i.e., the zero vector of required length.
+#' @description
+#' Approximation of correlated time series with given "Hurst" exponents. Internally \code{longmemo::simFGN0} or \code{longmemo::simFGN.fft} are used which simulate Gaussian series by generating fractional ARIMA(0,h,0) models (with $h=H-1/2$, \code{longmemo::FGN0}), or fractional Gaussian noise \code{longmemo::FGN.fft}. We cautiously note that we use empirical scaling (i.e., the variances are scaled to be 1 in the sample not the population), hence the between sample variance may be underrepresented. We further note that the covariance estimates for correlated time series (not using increments) is unstable.
+#'
 #' @return Returns a multivariate correlated time series with covariance matrix \code{Sigma}. The Hurst exponents are only approximate univariatly, since they result from mixed time series. Uncorrelated time series keep their univariate Hurst exponents \code{H}.
-#' @examples
-#' X <- simulate_cMTS(N = 100, H = .5, Sigma = matrix(c(1, .5, .5, 1), 2, 2))
+#'@examples
+#' Sigma <- matrix(.5, 3, 3); diag(Sigma) <- c(1,2,3)
+#' data <- simulate_cMTS(N = 10^5, Sigma = Sigma, H = c(.2, .5, .7),
+#'                       cor_increments = TRUE)
+#' cov(data)
+#' cov(apply(data,2,diff))
 #' @export
 
-simulate_cMTS <- function(N, H, Sigma, simulation_process = "FGN0", decomposition = "chol")
+simulate_cMTS <- function(N, H, Sigma, simulation_process = "FGN0", decomposition = "chol", cor_increments = TRUE, X0 = rep(0, ncol(Sigma)))
 {
      ### check input ---
      if(!simulation_process %in% c("FGN0", "FGN.fft")) stop("simulation_process can either be FGN0 or FGN.fft")
@@ -21,26 +30,43 @@ simulate_cMTS <- function(N, H, Sigma, simulation_process = "FGN0", decompositio
      if(length(H) == 1) H <- rep(H, d)
      if(length(H) != d) stop("dimensions of H and Sigma do not match!")
 
+     ### compute sqrt of Sigma:
+     if(tolower(decomposition) %in% c("chol", "cholesky")){
+          Sigma_sq <-  chol(Sigma)
+     }else if(tolower(decomposition) %in% c("eigen", "eigenvalue", "svd"))
+     {
+          ew <- eigen(Sigma)
+          Sigma_sq <- ew$vectors %*% diag(sqrt(ew$values)) %*% solve(ew$vectors)
+     }
+
      ### generate independent and unit variance variable
      if(simulation_process == "FGN0")
      {
-          Xi <- as.data.frame(scale(sapply(H, FUN = function(h)
-               longmemo::simFGN0(n = N, H = h))))
+          Xi <- sapply(H, FUN = function(h)
+               longmemo::simFGN0(n = N, H = h))
      }else if(simulation_process == "FGN.fft")
      {
-          Xi <- as.data.frame(scale(sapply(H, FUN = function(h)
-               longmemo::simFGN.fft(n = N, H = h))))
+          Xi <-sapply(H, FUN = function(h)
+               longmemo::simFGN.fft(n = N, H = h))
      }
 
 
      ### generate intercorrelated variables from independent ones
-     if(tolower(decomposition) %in% c("chol", "cholesky")){
-          X <- data.frame(as.matrix(Xi) %*% chol(Sigma))
+     if(cor_increments){
+          Xi <- as.matrix(scale(apply(Xi, 2, diff)))
      }else{
-          ew <- eigen(Sigma)
-          Sigma_sq <- ew$vectors %*% diag(sqrt(ew$values)) %*% solve(ew$vectors)
-          X <- data.frame(as.matrix(Xi) %*% Sigma_sq)
+          Xi <- as.matrix(scale(Xi))
      }
+
+     X_temp <- Xi %*% Sigma_sq
+
+     if(cor_increments){
+          X <- apply(rbind(X0, X_temp), 2, cumsum)
+     }else{
+          X <- X_temp
+     }
+
+     X <- data.frame(X)
      names(X) <- paste0("X", 1:d)
      return(X)
 }
